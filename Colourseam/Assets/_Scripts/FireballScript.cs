fdd42a0d3777fbdfd285408703969b6e3e24450b
Zac <zac.molyneux@hotmail.com>
2017-02-28 16:12:08 +1100

    started colourseam project in 2d.

---
+++ Colourseam/Assets/_Scripts/FireballScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballScript : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D Rb;
+
+    float durationTimer;
+    public float ignoreTime;
+
+    public Collider2D playerCollider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+        Rb.AddRelativeForce(Vector2.up * speed * Time.deltaTime);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+
+        durationTimer += Time.deltaTime;
+        if(durationTimer < ignoreTime)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), playerCollider, false);
+        }
+    }
+}
---
+++ Colourseam/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int player;
+    public float movementSpeed;
+
+    Rigidbody2D Rb;
+
+    float thumbstickDeadZone = 0.2f;
+    public float VelocityClamp = 5;
+
+    //for rotating player
+    private Vector3 lookDirection;
+    public float currentTurnSpeed;
+    
+
+
+    void Start ()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+	}
+	
+
+	void Update ()
+    {
+        ThumbstickMovement();
+        Rotate();
+    }
+
+    void ThumbstickMovement()
+    {
+        //move left or right
+        if (Controller.state[player].ThumbSticks.Left.X > thumbstickDeadZone)
+        {
+            if (Rb.velocity.x < VelocityClamp)
+            {
+                Rb.AddForce(Vector2.right * movementSpeed * Time.deltaTime);
+            }
+        }
+        else if (Controller.state[player].ThumbSticks.Left.X < -thumbstickDeadZone)
+        {
+            if (Rb.velocity.x > -VelocityClamp)
+            {
+                Rb.AddForce(Vector2.left * movementSpeed * Time.deltaTime);
+            }
+        }
+
+        //move up or down
+        if (Controller.state[player].ThumbSticks.Left.Y > thumbstickDeadZone)
+        {
+            if (Rb.velocity.y < VelocityClamp)
+            {
+                Rb.AddForce(Vector2.up * movementSpeed * Time.deltaTime);
+            }
+        }
+        else if (Controller.state[player].ThumbSticks.Left.Y < -thumbstickDeadZone)
+        {
+            if (Rb.velocity.y > -VelocityClamp)
+            {
+                Rb.AddForce(Vector2.down * movementSpeed * Time.deltaTime);
+            }
+        }
+    }
+    
+
+    void Rotate()
+    {
+        if (Mathf.Abs(Controller.state[player].ThumbSticks.Right.X) > thumbstickDeadZone || Mathf.Abs(Controller.state[player].ThumbSticks.Right.Y) > thumbstickDeadZone)
+        {
+            float heading = -Mathf.Atan2(Controller.state[player].ThumbSticks.Right.X, Controller.state[player].ThumbSticks.Right.Y);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, heading * Mathf.Rad2Deg), Time.deltaTime * currentTurnSpeed);
+        }
+    }
+}
